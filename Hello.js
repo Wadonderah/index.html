bc9d42074bb91a7d235bf9e26801dd03f1e15aef
Wadondera_2024 <wadondera@gmail.com>
2024-03-26 08:03:21 +0300

    commit

---
+++ Hello.js
@@ -0,0 +1,145 @@
+var name = "Wadondera A. Collins";
+
+console.log(name);
+let message = "Am a student";
+
+console.log(message);
+const name2 = "Wadondera A. Collins";
+
+console.log(name2);
+
+// Camel Case - first Name
+// Pascal Case - FirstName
+// Snake Case - first_name
+// Kebab Case - first-name
+// Screaming Snake Case - FIRST_NAME
+// Screaming Kebab Case - FIRST-NAME
+
+var success = true;
+var fail = false;
+
+if(success) {
+          console.log("success");
+}
+
+var int = 10;
+console.log(int);
+
+var float = 10.5;
+console.log(float);
+
+var int;
+console.log(int);
+
+let age = null;
+console.log(age)
+
+// addition +
+console.log(10 + 5);
+// subtraction -
+console.log(10 - 5);
+// multiplication *
+console.log(10 * 5);
+// division /
+console.log(10 / 5);
+// modulus %
+console.log(10 % 5);
+// exponent **
+console.log(10 ** 5);
+// increment ++
+console.log(10++);
+// decrement --
+console.log(10--);
+// exponentiation **
+console.log(2 ** 8);
+// remainder %
+console.log(10 % 3);
+
+var x = 100;
+var y = 200;
+     x + y;
+console.log(x);
+
+var x = 100;
+var y = 200;
+     x - y;
+console.log(x);
+
+var x = 100;
+var y = 200;
+     x * y;
+console.log(x);
+
+var x = 100;
+var y = 200;
+     x / y;
+console.log(x);
+
+var x = 100;
+var y = 200;
+     x % y;
+console.log(x);
+
+var x = 100;
+var y = 200;
+     x ** y;
+console.log(x);
+
+var x = 100;
+var y = 200;
+     x++;
+console.log(x);
+
+var x = 100;
+var y = 200;
+     x--;
+console.log(x);
+
+var x = 100;
+var y = 200;
+     x += y;
+console.log(x);
+
+
+// equal ==
+console.log(10 == 10);
+// not equal !=
+console.log(10 != 10);
+// greater than >
+console.log(10 > 10);
+// less than <
+console.log(10 < 10);
+// greater than or equal to >=
+console.log(10 >= 10);
+// less than or equal to <=
+console.log(10 <= 10);
+// and &&
+console.log(10 && 10);
+// or ||
+console.log(10 || 10);
+
+// and &&
+var x = 100;
+var y= 50;
+if(x == 100 && y == 50){
+          console.log("true");
+} else {
+          console.log("false");
+}
+
+var balance = 1000;
+if{
+     (balance > 1000){
+          console.log("you can purchase this item,");
+     } else if(balance < 1000)
+     console.log("you have insufficient balance. ")
+} else {
+     console.log('you have just enough')
+}
+
+
+
+
+
+     
+}
